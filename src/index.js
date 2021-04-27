4b3c175bc6f4c7126801d26b3fccf4994281e239
金 流星 <yuson.kim@glv.co.jp>
2021-04-27 23:33:45 +0900

    refactor

--- src/index.js
+++ src/index.js
@@ -4,27 +4,36 @@ const addButton = document.getElementById("add")
 const minusButton = document.getElementById("minus")
 const counter = document.querySelector("span")
 
+counter.innerText = 0
+
+const ADD = "ADD"
+const MINUS = "MINUS"
+
 // reducer
 const countModifier = (state = 0, action) => {
   console.log("current state: ", state, action)
-  if (action.type === "ADD") {
-    return state + 1
+  switch (action.type) {
+    case ADD:
+      return state + 1
+    case MINUS:
+      return state - 1
+    default:
+      return state
   }
-  if (action.type === 'MINUS') {
-    return state - 1
-  }
-  return state
 }
 
 // store
 const countStore = createStore(countModifier)
 
 // send message to the store
-const handleAdd = () => countStore.dispatch({type: "ADD"})
-const handleMinus = () => countStore.dispatch({type: "MINUS"})
+const handleAdd = () => countStore.dispatch({type: ADD})
+const handleMinus = () => countStore.dispatch({type: MINUS})
 const onButtonClick = () => counter.innerText = countStore.getState()
 
+// add listener to buttons
 addButton.addEventListener("click", handleAdd)
 minusButton.addEventListener("click", handleMinus)
+
+// subscribe any change of state on store
 countStore.subscribe(onButtonClick)
 
